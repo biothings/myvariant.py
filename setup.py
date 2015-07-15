1ef23909127280e9f16b9eb1ae770c15d54deeaf
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-07-15 09:56:40 -0700

    Making myvariant python client a package

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="myvariant",
+    version="0.1.0",
+    author="Chunlei Wu",
+    author_email="cwu@scripps.edu",
+    description="Python Client for MyVariant.Info services.",
+    license="BSD",
+    keywords="biology genetic variant annotation web service client api",
+    url="https://github.com/Network-of-BioThings/myvariant.py",
+    packages=['myvariant'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'httplib2>=0.6',
+    ],
+)
