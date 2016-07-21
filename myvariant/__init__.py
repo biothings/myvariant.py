392cbb61f10f38c55620871ce5d5c267bd635b1e
Chunlei Wu <anewgene@yahoo.com>
2016-07-21 13:17:28 -0700

    improved format_hgvs helper function for handling some edge cases in VCF file.

--- myvariant/__init__.py
+++ myvariant/__init__.py
@@ -124,6 +124,41 @@ def get_hgvs_from_vcf(input_vcf):
             yield format_hgvs(row[0], row[1], row[3], row[4])
 
 
+def _normalized_vcf(chrom, pos, ref, alt):
+    """If both ref/alt are > 1 base, and there are overlapping from the left,
+       we need to trim off the overlapping bases.
+       In the case that ref/alt is like this:
+           CTTTT/CT    # with >1 overlapping bases from the left
+       ref/alt should be normalized as TTTT/T, more examples:
+            TC/TG --> C/G
+       and pos should be fixed as well.
+    """
+    for i in range(max(len(ref), len(alt))):
+        _ref = ref[i] if i < len(ref) else None
+        _alt = alt[i] if i < len(alt) else None
+        if _ref is None or _alt is None or _ref != _alt:
+            break
+
+    # _ref/_alt cannot be both None, if so,
+    # ref and alt are exactly the same,
+    # something is wrong with this VCF record
+    assert not (_ref is None and _alt is None)
+
+    _pos = int(pos)
+    if _ref is None or _alt is None:
+        # if either is None, del or ins types
+        _pos = _pos + i - 1
+        _ref = ref[i-1:]
+        _alt = alt[i-1:]
+    else:
+        # both _ref/_alt are not None
+        _pos = _pos + i
+        _ref = ref[i:]
+        _alt = alt[i:]
+
+    return (chrom, _pos, _ref, _alt)
+
+
 def format_hgvs(chrom, pos, ref, alt):
     '''get a valid hgvs name from VCF-style "chrom, pos, ref, alt" data.
 
@@ -147,12 +182,15 @@ def format_hgvs(chrom, pos, ref, alt):
         if ref[0] == alt:
             start = int(pos) + 1
             end = int(pos) + len(ref) - 1
-            hgvs = 'chr{0}:g.{1}_{2}del'.format(chrom, start, end)
+            if start == end:
+                hgvs = 'chr{0}:g.{1}del'.format(chrom, start)
+            else:
+                hgvs = 'chr{0}:g.{1}_{2}del'.format(chrom, start, end)
         else:
             end = int(pos) + len(ref) - 1
             hgvs = 'chr{0}:g.{1}_{2}delins{3}'.format(chrom, pos, end, alt)
     elif len(ref) == 1 and len(alt) > 1:
-        # this is a insertion
+        # this is an insertion
         if alt[0] == ref:
             hgvs = 'chr{0}:g.{1}_{2}ins'.format(chrom, pos, int(pos) + 1)
             ins_seq = alt[1:]
@@ -160,8 +198,13 @@ def format_hgvs(chrom, pos, ref, alt):
         else:
             hgvs = 'chr{0}:g.{1}delins{2}'.format(chrom, pos, alt)
     elif len(ref) > 1 and len(alt) > 1:
-        end = int(pos) + len(alt) - 1
-        hgvs = 'chr{0}:g.{1}_{2}delins{3}'.format(chrom, pos, end, alt)
+        if ref[0] == alt[0]:
+            # if ref and alt overlap from the left, trim them first
+            _chrom, _pos, _ref, _alt = _normalized_vcf(chrom, pos, ref, alt)
+            return format_hgvs(_chrom, _pos, _ref, _alt)
+        else:
+            end = int(pos) + len(alt) - 1
+            hgvs = 'chr{0}:g.{1}_{2}delins{3}'.format(chrom, pos, end, alt)
     else:
         raise ValueError("Cannot convert {} into HGVS id.".format((chrom, pos, ref, alt)))
     return hgvs
@@ -304,13 +347,13 @@ class MyVariantInfo:
         return self._get(_url)
 
     def set_caching(self, cache_db='myvariant_cache', **kwargs):
-        ''' Installs a local cache for all requests.  
+        ''' Installs a local cache for all requests.
 
             **cache_db** is the path to the local sqlite cache database.'''
         if caching_avail:
             requests_cache.install_cache(cache_name=cache_db, **kwargs)
             self._cached = True
-        else: 
+        else:
             print("Error: The requests_cache python module is required to use request caching.")
             print("See - https://requests-cache.readthedocs.io/en/latest/user_guide.html#installation")
         return
