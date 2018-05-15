48c989fce1ad0b24c3f58749e10f0cc38a2963c0
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2018-05-15 12:01:24 -0700

    Mapping instance methods to module root.  All nosetests pass now

--- myvariant/__init__.py
+++ myvariant/__init__.py
@@ -3,6 +3,8 @@ Python Client for MyVariant.Info services
 '''
 from biothings_client import get_client
 
+__version__ = '0.3.1'
+
 class MyVariantInfo(get_client('variant', instance=False)):
     '''This is the client for MyVariant.info web services.
     Example:
@@ -11,3 +13,13 @@ class MyVariantInfo(get_client('variant', instance=False)):
 
     '''
     pass
+
+def format_hgvs(chrom, pos, ref, alt):
+    mv = MyVariantInfo()
+    return mv.format_hgvs(chrom=chrom, pos=pos, ref=ref, alt=alt)
+format_hgvs.__doc__ = MyVariantInfo.format_hgvs.__doc__
+
+def get_hgvs_from_vcf(input_vcf):
+    mv = MyVariantInfo()
+    return mv.get_hgvs_from_vcf(input_vcf=input_vcf)
+get_hgvs_from_vcf.__doc__ = MyVariantInfo.get_hgvs_from_vcf.__doc__
