892f72a9d7e89e2b1541b66f85594a3da2c6ef31
Chunlei Wu <anewgene@yahoo.com>
2018-12-17 14:19:35 -0800

    updated doc_strings

--- myvariant/__init__.py
+++ myvariant/__init__.py
@@ -3,7 +3,7 @@ Python Client for MyVariant.Info services
 '''
 from biothings_client import get_client
 
-__version__ = '0.3.1'
+__version__ = '1.0.0'
 
 class MyVariantInfo(get_client('variant', instance=False)):
     '''This is the client for MyVariant.info web services.
@@ -15,11 +15,17 @@ class MyVariantInfo(get_client('variant', instance=False)):
     pass
 
 def format_hgvs(chrom, pos, ref, alt):
+    """:py:func:`format_hgvs` helper function is now moved into :py:class:`MyVariantInfo` class
+       as a method: :py:meth:`format_hgvs`.
+    """
     mv = MyVariantInfo()
     return mv.format_hgvs(chrom=chrom, pos=pos, ref=ref, alt=alt)
-format_hgvs.__doc__ = MyVariantInfo.format_hgvs.__doc__
+# format_hgvs.__doc__ = MyVariantInfo.format_hgvs.__doc__
 
 def get_hgvs_from_vcf(input_vcf):
+    """:py:func:`get_hgvs_from_vcf` helper function is now moved into :py:class:`MyVariantInfo` class
+       as a method: :py:meth:`get_hgvs_from_vcf`.
+    """
     mv = MyVariantInfo()
     return mv.get_hgvs_from_vcf(input_vcf=input_vcf)
-get_hgvs_from_vcf.__doc__ = MyVariantInfo.get_hgvs_from_vcf.__doc__
+# get_hgvs_from_vcf.__doc__ = MyVariantInfo.get_hgvs_from_vcf.__doc__
